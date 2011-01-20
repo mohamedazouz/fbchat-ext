512054074c40ceea13e2f83372e67f51d5a2727e
gemy21ce@gmail.com <gemy21ce@gmail.com@1c266af5-ceb8-c38f-eb94-390531f5c185>
2011-01-20 13:07:13 +0000

    packaging.

---
+++ src/java/com/activedd/google/extensions/fbchat/chat/FriendBuddy.java
@@ -0,0 +1,73 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.activedd.google.extensions.fbchat.chat;
+
+/**
+ *
+ * @author Activedd2
+ */
+public class FriendBuddy {
+    private String id;
+    private String name;
+    private String staus;
+    private String pic;
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the staus
+     */
+    public String getStaus() {
+        return staus;
+    }
+
+    /**
+     * @param staus the staus to set
+     */
+    public void setStaus(String staus) {
+        this.staus = staus;
+    }
+
+    /**
+     * @return the pic
+     */
+    public String getPic() {
+        return pic;
+    }
+
+    /**
+     * @param pic the pic to set
+     */
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+    
+}

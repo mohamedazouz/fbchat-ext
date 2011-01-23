debac5c7482bc8577233f033ab5ebb376b43503b
gemy21ce@gmail.com <gemy21ce@gmail.com@1c266af5-ceb8-c38f-eb94-390531f5c185>
2011-01-23 15:28:14 +0000

    using spring 2.5

---
+++ src/java/com/activedd/google/extensions/fbchat/controller/Messaging.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.activedd.google.extensions.fbchat.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+/**
+ *
+ * @author prog
+ */
+public class Messaging extends MultiActionController{
+
+    public void send(HttpServletRequest request,HttpServletResponse response){
+        //TO DO: send a message,
+    }
+
+    public void onlinefriends(HttpServletRequest request,HttpServletResponse response){
+        //TO DO: get list of online friends.
+    }
+}

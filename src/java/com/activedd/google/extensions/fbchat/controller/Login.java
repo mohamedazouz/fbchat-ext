427194751765f9989653ecf4531f9ef8829cb79c
gemy21ce@gmail.com <gemy21ce@gmail.com@1c266af5-ceb8-c38f-eb94-390531f5c185>
2011-01-23 15:28:33 +0000

    using spring 2.5

---
+++ src/java/com/activedd/google/extensions/fbchat/controller/Login.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.activedd.google.extensions.fbchat.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+/**
+ *
+ * @author prog
+ */
+public class Login extends MultiActionController{
+
+    /**
+     * //write what this url does here.
+     * @param request
+     * @param response
+     */
+    public void login(HttpServletRequest request,HttpServletResponse response){
+        //TO DO: redirect to the login screen in facebook.
+    }
+
+    /**
+     * //write what this url does here.
+     * @param request
+     * @param response
+     */
+    public void authenticate(HttpServletRequest request,HttpServletResponse response){
+        //TO DO: this url that will facebook redirects to after authenticating application from facebook.
+        //      and set the user key in the httpsession.
+    }
+
+    public void getauthkey(HttpServletRequest request,HttpServletResponse response){
+        //TO DO: check if the user has authenticated from facebook by checking http session and if he does, then populate the user key/id in the respose and delete it from http session.
+    }
+
+}

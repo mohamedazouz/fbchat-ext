f15da2cbf16428588acc8db36c8078bb59e7a8ea
mohamedaliazouz <mohamedaliazouz@1c266af5-ceb8-c38f-eb94-390531f5c185>
2011-01-24 09:08:31 +0000

    added implmentation for the following 1-login.java which  contain login ,auth and getauthkey url 2-connect.java which contain connect and disconnect url 3-messagesing which contain online and send message url

--- src/java/com/activedd/google/extensions/fbchat/controller/Connect.java
+++ src/java/com/activedd/google/extensions/fbchat/controller/Connect.java
@@ -2,25 +2,45 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.activedd.google.extensions.fbchat.controller;
 
+import com.activedd.google.extensions.fbchat.chat.ChatClient;
+import com.google.code.facebookapi.FacebookException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.jivesoftware.smack.XMPPException;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 /**
  *
  * @author prog
  */
-public class Connect extends MultiActionController{
+public class Connect extends MultiActionController {
 
+    HttpSession session;
 
-    public void connect(HttpServletRequest request,HttpServletResponse response){
-        //TO DO: go online on facebook.
+    public void connect(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            //TO DO: go online on facebook.
+            //get the seesion key from url as parameter
+            session = request.getSession();
+            ChatClient c = new ChatClient();
+            String sessionkey = request.getParameter("sessionkey");
+            c.login(sessionkey);
+            session.setAttribute("buddList", c);
+        } catch (Exception ex) {
+            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
-    public void disconnect(HttpServletRequest request,HttpServletResponse response){
+    public void disconnect(HttpServletRequest request, HttpServletResponse response) {
         //TO DO: go offline.
+        session = request.getSession();
+        ChatClient c = (ChatClient) session.getAttribute("buddList");
+        c.disconnect();
+        session.removeAttribute("buddList");
     }
 }
--- src/java/com/activedd/google/extensions/fbchat/controller/Login.java
+++ src/java/com/activedd/google/extensions/fbchat/controller/Login.java
@@ -2,27 +2,43 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.activedd.google.extensions.fbchat.controller;
 
+import com.google.code.facebookapi.FacebookException;
+import com.google.code.facebookapi.FacebookJsonRestClient;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 /**
  *
  * @author prog
  */
-public class Login extends MultiActionController{
+public class Login extends MultiActionController {
+
+    HttpSession session;
+    private FacebookJsonRestClient facebook;
+    private String apiKey = "76f98c6f348e8d27ed504ae74da69cea";  //Application Key
+    private String apiSecret = "c4cc0e40e6f8f17362685640a9b0adb4";  //Application Secert key
 
     /**
      * //write what this url does here.
      * @param request
      * @param response
      */
-    public void login(HttpServletRequest request,HttpServletResponse response){
-        System.out.println("helllooooooooo");
-        //TO DO: redirect to the login screen in facebook.
+    public void login(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String redirct = "http://www.facebook.com/login.php?api_key=172430629459688&connect_display=popup&v=1.0&next=http://www.facebook.com/login.php?api_key=76f98c6f348e8d27ed504ae74da69cea&v=1.0/&cancel_url=http://www.facebook.com/connect/login_failure.html&fbconnect=true&return_session=true&session_key_only=true&req_perms=user_photos,user_videos,publish_stream,status_update,xmpp_login,offline_access";
+            response.sendRedirect(redirct);
+            //TO DO: redirect to the login screen in facebook.
+
+        } catch (Exception ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -30,13 +46,28 @@ public class Login extends MultiActionController{
      * @param request
      * @param response
      */
-    public void authenticate(HttpServletRequest request,HttpServletResponse response){
-        //TO DO: this url that will facebook redirects to after authenticating application from facebook.
-        //      and set the user key in the httpsession.
+    public void authenticate(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            //TO DO: this url that will facebook redirects to after authenticating application from facebook.
+            //      and set the user key in the httpsession.
+            session = request.getSession();
+            String token = request.getParameter("auth_token");
+            facebook = new FacebookJsonRestClient(apiKey, apiSecret);
+            String FB_SESSION_KEY = facebook.auth_getSession(token);
+            session.setAttribute("sessionkey", FB_SESSION_KEY);
+        } catch (Exception ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
-    public void getauthkey(HttpServletRequest request,HttpServletResponse response){
+    public void getauthkey(HttpServletRequest request, HttpServletResponse response) {
         //TO DO: check if the user has authenticated from facebook by checking http session and if he does, then populate the user key/id in the respose and delete it from http session.
-    }
+        if (session.getAttribute("sessionkey") != null) {
+            String sessionkey = (String) session.getAttribute("sessionkey");
+            session.removeAttribute("sessionkey");
+            response.setHeader("sessionkey", sessionkey);
+        }
 
+
+    }
 }
